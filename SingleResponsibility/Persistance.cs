7b2e0869142a0030b69967b8219969ce6e9ebcca
Aesir1 <bryan.fatjo@gmail.com>
2023-03-30 11:58:00 +0200

    SinlgeResponsibility

---
+++ SingleResponsibility/Persistance.cs
@@ -0,0 +1,17 @@
+﻿namespace SingleResponsability;
+
+public class Persistance
+{
+    public void SaveToFile(Journal journal, string fileName, bool overwrite = false)
+    {
+        if(overwrite || !File.Exists(fileName))
+            File.WriteAllText(fileName, journal.ToString());
+    }
+
+    public string Load(string fileName)
+    {
+        string loadedFile = File.ReadAllText(fileName).ToString();
+
+        return loadedFile;
+    }
+}
---
+++ SingleResponsibility/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SingleResponsability;
+
+Journal journal = new Journal();
+journal.AddEntry("First entry");
+journal.AddEntry("second entry");
+
+
+Persistance persistance = new Persistance();
+string fileName = @"C:\Users\Public\Documents\journal.txt";
+persistance.SaveToFile(journal, fileName, true);
+Console.WriteLine(persistance.Load(fileName));
