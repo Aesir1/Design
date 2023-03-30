c2c69b48b9648752b0d17ac2adf438b612c41e3b
Aesir1 <bryan.fatjo@gmail.com>
2023-03-30 11:58:00 +0200

    SinlgeResponsibility

---
+++ SingleResponsibility/Journal.cs
@@ -0,0 +1,23 @@
+﻿namespace SingleResponsability;
+
+public class Journal
+{
+    private static int count;
+    private readonly List<string> entries = new();
+
+    public int AddEntry(string text)
+    {
+        entries.Add($"{++count}: {text}");
+        return count;
+    }
+
+    public void RemoveEntry(int index)
+    {
+        entries.RemoveAt(index);
+    }
+
+    public override string ToString()
+    {
+        return string.Join(Environment.NewLine, entries);
+    }
+}
