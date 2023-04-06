29d1e198c5a452c75c6e9e11c6ab482368d6c963
Aesir1 <bryan.fatjo@gmail.com>
2023-04-06 12:52:41 +0200

    LiskovSubstitution

---
+++ LiskovSubstitution/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LiskovSubstitution;
+
+static int Area(Rectangle r) => r.Width * r.Height;
+
+Rectangle rectangle = new Rectangle(2,3);
+Console.WriteLine($"{rectangle} has area {Area(rectangle)}");
+
+Rectangle square = new Square
+{
+    Width = 4
+};
+Console.WriteLine($"{square} has area {Area(square)}");
---
+++ LiskovSubstitution/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿namespace LiskovSubstitution;
+
+public class Rectangle
+{
+    public virtual int Width { get; set; }
+    public virtual int Height { get; set; }
+ 
+    public Rectangle(){}
+    public Rectangle(int height, int width)
+    {
+        Height = height;
+        Width = width;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height} ";
+    }
+}
