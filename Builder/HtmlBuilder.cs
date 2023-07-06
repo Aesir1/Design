baa5030bd2e928cfec6764d62a7780461b0bc458
Aesir1 <bryan.fatjo@gmail.com>
2023-07-06 16:24:45 +0200

    Builder

---
+++ Builder/HtmlBuilder.cs
@@ -0,0 +1,35 @@
+﻿namespace Builder;
+
+public class HtmlBuilder
+{
+    private HtmlElement root = new HtmlElement();
+    private readonly string rootName;
+
+
+    public HtmlBuilder(string rootName)
+    {
+        this.rootName = rootName;
+        root.Name = this.rootName;
+    }
+
+    // Fluent builder or Fluent Interface
+    public HtmlBuilder AddChild(string childName, string childText)
+    {
+        var e = new HtmlElement(childName, childText);
+        root.Elements.Add(e);
+        return this;
+    }
+
+    public override string ToString()
+    {
+        return root.ToString();
+    }
+
+    public void Clear()
+    {
+        root = new HtmlElement()
+        {
+            Name = rootName
+        };
+    } 
+}
--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Builder;
+
 Console.WriteLine("Hello, World!");
+
+var builder = new HtmlBuilder("ul");
+builder.AddChild("li", "Hello").AddChild("li", "World");
+Console.WriteLine(builder.ToString());
